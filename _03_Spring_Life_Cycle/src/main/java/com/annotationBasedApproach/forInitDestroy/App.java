0e42329e19df7f4463eef704efc6a973985d75b9
Deepankar singh <singhdeepankarpost@gmail.com>
2025-04-02 13:17:21 +0530

    Anntation-not-working

--- _03_Spring_Life_Cycle/src/main/java/com/annotationBasedApproach/forInitDestroy/App.java
+++ _03_Spring_Life_Cycle/src/main/java/com/annotationBasedApproach/forInitDestroy/App.java
@@ -1,24 +1,24 @@
-//package com.annotationBasedApproach.forInitDestroy;
-//
-//import org.springframework.context.support.AbstractApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//
-//import com.InterfaceApproach.forInitDestroy.Student;
+package com.annotationBasedApproach.forInitDestroy;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.InterfaceApproach.forInitDestroy.Student;
 /*
  * @postconstruct and @preDestroy not working due to depreciation
  * It works on java 9 only not on above versions sadly
  * So , i am commiting the code for reference but this package code will not work
  * 
  */
-//public class App {
-//
-//	public static void main(String[] args) {
-//		
-//		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/annotationBasedApproach/forInitDestroy/config.xml");
-//		 context.registerShutdownHook();
-//		 
-//		 Students stu=context.getBean("StudentBean",Students.class);
-//		 
-//		 System.out.println(stu);
-//	}
-//}
+public class App {
+
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/annotationBasedApproach/forInitDestroy/config.xml");
+		 context.registerShutdownHook();
+		 
+		 Students stu=context.getBean("StudentBean",Students.class);
+		 
+		 System.out.println(stu);
+	}
+}
